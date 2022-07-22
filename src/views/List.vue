b28695db439e02f83633316a87b76c38195b574c
chenxiang <328692993@qq.com>
2022-07-22 09:44:11 +0800

    添加用户

--- src/views/List.vue
+++ src/views/List.vue
@@ -6,12 +6,15 @@
 
     >
         <template #append>
-            <el-button @click="getData()"><el-icon style="vertical-align: middle">
-                <Search />
-            </el-icon></el-button>
+            <el-button @click="getData();">
+                <el-icon style="vertical-align: middle">
+                    <Search/>
+                </el-icon>
+            </el-button>
         </template>
     </el-input>
-    <el-button type="primary" @click="dialogVisible = true;handleClick($event)" style="margin-left: 20px;width:60px;height:30px;">
+    <el-button type="primary" @click="addTest(); handleClick($event);"
+               style="margin-left: 20px;width:60px;height:30px;">
         新增
     </el-button>
     <el-table :data="datas" style="margin-top:20px;">
@@ -37,31 +40,31 @@
     >
         <el-form :model="formData" label-width="120px" :rules="rules" ref="form">
             <el-form-item label="姓名"
-                           prop="name">
-                <el-input v-model="formData.name" />
+                          prop="name">
+                <el-input v-model="formData.name"/>
             </el-form-item>
             <el-form-item label="性别">
                 <el-radio-group v-model="formData.sex">
-                    <el-radio label="男" />
-                    <el-radio label="女" />
+                    <el-radio label="男"/>
+                    <el-radio label="女"/>
                 </el-radio-group>
             </el-form-item>
             <el-form-item label="是否结婚">
                 <el-radio-group v-model="formData.isMarry">
-                    <el-radio label="false" >否</el-radio>
-                    <el-radio label="true" >是</el-radio>
+                    <el-radio label="false">否</el-radio>
+                    <el-radio label="true">是</el-radio>
                 </el-radio-group>
             </el-form-item>
             <el-form-item label="部门" prop="department">
-                <el-input v-model="formData.department" />
+                <el-input v-model="formData.department"/>
             </el-form-item>
-            <el-form-item label="年龄"  prop="age">
-                <el-input type="number" v-model="formData.age" />
+            <el-form-item label="年龄" prop="age">
+                <el-input type="number" v-model="formData.age"/>
             </el-form-item>
         </el-form>
         <template #footer>
       <span class="dialog-footer">
-        <el-button @click="dialogVisible = false;this.$refs.formData.resetFields();">取消</el-button>
+        <el-button @click="dialogVisible = false;">取消</el-button>
         <el-button type="primary" @click="save()"
         >保存</el-button
         >
@@ -73,25 +76,26 @@
 <script>
     import {testList} from "../common/api.js";
     import {save} from "../common/api.js";
+
     export default {
         name: 'List',
         data() {
             return {
-                formData:{sex:'男',isMarry:'false'},
-                dialogVisible:false,
+                formData: {sex: '男', isMarry: 'false'},
+                dialogVisible: false,
                 searchKey: '',
                 pages: 1,
                 size: 10,
                 total: 0,
                 datas: [],
-                rules : {
+                rules: {
                     name: [
-                        { required: true, message: '请填写姓名', trigger: 'blur' },
-                        { min: 2, max: 10, message: '字符长度2-10', trigger: 'blur' },
+                        {required: true, message: '请填写姓名', trigger: 'blur'},
+                        {min: 2, max: 10, message: '字符长度2-10', trigger: 'blur'},
                     ],
                     department: [
-                        { required: true, message: '请填写部门', trigger: 'blur' },
-                        { min: 1, max: 30, message: '字符长度1-30', trigger: 'blur' },
+                        {required: true, message: '请填写部门', trigger: 'blur'},
+                        {min: 1, max: 30, message: '字符长度1-30', trigger: 'blur'},
                     ]
                 }
             }
@@ -108,22 +112,29 @@
                 this.getData();
                 console.log(this.size);
             },
+            addTest() {
+                this.dialogVisible = true;
+                if (this.$refs.form) {
+                    this.$refs.form.resetFields();
+                }
+            },
             handleClick(e) {
                 let target = e.target;
-                if(target.nodeName == 'SPAN' || target.nodeName == 'I'){
+                if (target.nodeName == 'SPAN' || target.nodeName == 'I') {
                     target = e.target.parentNode;
                 }
                 target.blur();
-            },save(){
+            }, save() {
                 this.$refs.form.validate(async (valid) => {
                     if (valid) {
-                        console.log("form",this.formData);
-                        console.log("form1",this.formData.Target);
+                        console.log("form", this.formData);
+                        console.log("form1", this.formData.Target);
                         let data = JSON.parse(JSON.stringify(this.formData));
-                        console.log("form2",data);
+                        console.log("form2", data);
                         await save(data);
                         this.dialogVisible = false;
                         this.getData();
+
                     }
                 })
             },
