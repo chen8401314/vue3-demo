a0a74f6ae7cb94a36b5e7b6af7d7946071cd4e26
chenxiang <328692993@qq.com>
2025-04-28 14:33:50 +0800

    左侧菜单添加抽屉显示方式

--- src/views/Aside.vue
+++ src/views/Aside.vue
@@ -1,45 +1,81 @@
 <template>
+  <!-- 添加动态宽度和过渡效果 -->
+  <el-container
+      :style="{
+      width: isCollapse ? '64px' : '200px',
+      transition: 'width 0.3s ease',
+
+    }"
+      style="height:100%;overflow:hidden;"
+  >
+    <el-header height="40px" style="display: flex; align-items: center;">
+      <!-- 添加点击事件 -->
+      <el-icon
+          style="margin-left: auto; cursor: pointer;"
+          @click="toggleCollapse"
+      >
+        <d-arrow-left :class="{ 'rotate-icon': isCollapse }"/>
+      </el-icon>
+    </el-header>
     <el-menu
-            class="el-menu-vertical-demo"
-            :default-active="defaultPath"
-            @open="handleOpen"
-            @close="handleClose"
-            unique-opened="true"
-            router
-    >
-        <el-sub-menu index="1">
-            <template #title>
-                <el-icon>
-                    <Operation/>
-                </el-icon>
-                <span>菜单一</span>
-            </template>
-            <el-sub-menu index="1-1">
-                <template #title>子菜单1</template>
-                <el-menu-item index="/list">子菜单1-1</el-menu-item>
-                <el-menu-item index="/test">子菜单1-2</el-menu-item>
-            </el-sub-menu>
+        unique-opened="true"
+        :collapse="isCollapse"
+        :collapse-transition="false"
+        :default-active="defaultPath"
+        router>
+      <el-sub-menu index="1">
+        <template #title>
+          <el-icon>
+            <Operation/>
+          </el-icon>
+          <span>菜单一</span>
+        </template>
+        <el-sub-menu index="1-1">
+          <template #title>子菜单1</template>
+          <el-menu-item index="/list">子菜单1-1</el-menu-item>
+          <el-menu-item index="/test">子菜单1-2</el-menu-item>
         </el-sub-menu>
+      </el-sub-menu>
+      <el-sub-menu>
+        <template #title>
+          <el-icon>
+            <Operation/>
+          </el-icon>
+          <span>菜单二</span>
+        </template>
         <el-sub-menu>
-            <template #title>
-                <el-icon>
-                    <Operation/>
-                </el-icon>
-                <span>菜单二</span>
-            </template>
-            <el-sub-menu>
-                <template #title>子菜单2</template>
-                <el-menu-item index="/list1">子菜单2-1</el-menu-item>
-                <el-menu-item index="/test1">子菜单2-2</el-menu-item>
-            </el-sub-menu>
+          <template #title>子菜单2</template>
+          <el-menu-item index="/list1">子菜单2-1</el-menu-item>
+          <el-menu-item index="/test1">子菜单2-2</el-menu-item>
         </el-sub-menu>
+      </el-sub-menu>
     </el-menu>
+  </el-container>
+
 
 </template>
 <script setup>
-    import {onMounted, ref} from "vue";
-    const defaultPath = ref('/list');
+import {ref} from "vue";
+
+const defaultPath = ref('/list');
+const isCollapse = ref(false);
+
+const emit = defineEmits(['collapse-change'])
+const toggleCollapse = () => {
+  isCollapse.value = !isCollapse.value
+  emit('collapse-change', isCollapse.value) // 新增此行
+}
+
+
 
 </script>
-<style>
+<style>/* 添加图标旋转动画 */
+.rotate-icon {
+  transform: rotate(180deg);
+  transition: transform 0.3s;
+}
+/* 图标间距 */
+.el-menu-item > i, .el-sub-menu__title > i {
+  margin-right: 8px;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,31 @@
         <el-header height="80px" style="background:#c2ece4;">
             <Head v-if="isInit"/>
         </el-header>
-        <el-container style="background:aliceblue;">
-            <el-aside style="height:100%;">
-                <el-scrollbar :max-height="maxAsideHeight" style="background: white">
-                    <Aside/>
-                </el-scrollbar>
-            </el-aside>
-            <el-main>
-                <el-scrollbar :max-height="maxHeight" v-if="isInit">
-                    <router-view></router-view>
-                </el-scrollbar>
-            </el-main>
+      <el-container style="flex:1;background:aliceblue;">
+          <!-- 添加动态宽度和过渡 -->
+          <el-aside
+              :style="{
+          width: asideWidth,
+          position: 'relative', // 关键修改
+          transition: 'width 0.3s'
+        }"
+          >
+            <el-scrollbar :max-height="maxAsideHeight" style="background: white">
+              <Aside @collapse-change="handleCollapse"/>
+            </el-scrollbar>
+          </el-aside>
+
+        <!-- 主内容区域改用动态padding -->
+        <el-main
+            :style="{
+          transition: 'padding 0.3s',
+          width: '100%' // 新增宽度约束
+        }"
+        >
+            <el-scrollbar :max-height="maxHeight" v-if="isInit">
+              <router-view></router-view>
+            </el-scrollbar>
+          </el-main>
         </el-container>
     </el-container>
 </template>
@@ -22,11 +36,14 @@
     import Aside from '@/views/Aside.vue'
     import {useStore} from "vuex";
     import {onMounted, ref} from "vue";
-    import {useRouter} from "vue-router";
-    import {ElMessage} from "element-plus";
 
-    const router = useRouter();
-
+    const handleCollapse = (state) => {
+      isCollapse.value = state
+      asideWidth.value = state ? '64px' : '200px'
+    }
+    // 添加折叠状态管理
+    const isCollapse = ref(false)
+    const asideWidth = ref('200px')
     const maxAsideHeight = ref('0')
     const maxHeight = ref('0')
     const isInit = ref(false)
@@ -35,7 +52,6 @@
     const init = async () => {
         await store.dispatch("getUserInfo");
         isInit.value = true;
-
     }
     onMounted(() => {
         init();
@@ -50,12 +66,24 @@
     })
 </script>
 <style>
-    .el-pagination {
-        background: white;
-        padding: 5px;
-        padding-left: 10px;
-        border: 1px solid var(--el-border-color-lighter);
-        border-top: 0px;
-    }
+.main-container {
+  display: flex;
+  height: 100vh;
+}
+.content {
+  flex: 1;
+  transition: margin-left 0.3s ease;
+}
 
+.el-container.el-container--default {
+  display: flex !important;
+}
+.el-main {
+  flex: 1;
+  min-width: 0; /* 修复flex布局溢出问题 */
+}
+/* 确保滚动条正常显示 */
+.el-scrollbar__wrap {
+  overflow-x: hidden !important;
+}
 </style>
